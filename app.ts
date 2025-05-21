cfda6cac976460787f9242120f5ea2fd53511bad
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-05-21 09:27:19 +0200

    feature: change responsabilities

--- app.ts
+++ app.ts
@@ -7,6 +7,8 @@ import contactController from "./controllers/contact";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 // Montar las rutas del controlador
 app.use(
   "/",
@@ -15,8 +17,9 @@ app.use(
   usersController,
   contactController
 );
-app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
+
+export default app;
