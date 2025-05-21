a01627f1e5e5aa7de5424f48c026b58d7645c6a4
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-05-21 09:09:13 +0200

    fix: added controller to index

--- index.ts
+++ index.ts
@@ -2,12 +2,19 @@ import express from "express";
 import bookingsController from "./controllers/booking";
 import roomsController from "./controllers/room";
 import usersController from "./controllers/user";
+import contactController from "./controllers/contact";
 
 const app = express();
 const port = 3000;
 
 // Montar las rutas del controlador
-app.use("/", bookingsController, roomsController, usersController);
+app.use(
+  "/",
+  bookingsController,
+  roomsController,
+  usersController,
+  contactController
+);
 app.use(express.json());
 
 app.listen(port, () => {
